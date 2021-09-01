81147608f0b624bdc5de7856b71abd5d10fe57a7
Moataz-Habib <89359615+Moataz-Habib@users.noreply.github.com>
2021-09-01 02:56:58 +0200

    Add files via upload

---
+++ STL Algorithms/UVA 10107.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#define endl '\n'
+using namespace std;
+int main() {
+	int x;
+	vector<int>v;
+	while (cin >> x)
+	{
+		v.push_back(x);
+		sort(v.begin(), v.end());
+		if (v.size() & 1)cout << v[v.size() / 2] << endl;
+		else cout << (v[v.size() / 2 ]+ v[v.size()/2- 1])/2 << endl;
+	}
+}
